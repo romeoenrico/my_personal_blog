1ced5a4b84b010505274ec718a99a347c8e96ab9
Enrico Romeo <enri.rome@yahoo.it>
2019-01-17 12:47:16 +0100

    node modules fix

--- resources/views/frontend/master/layout.blade.php
+++ resources/views/frontend/master/layout.blade.php
@@ -92,8 +92,6 @@
 
 
     <script src="{{ asset('js/jsresources.js') }}"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-    <script src="{{ asset('todeletejs/google-map.js') }}"></script>
 
   </body>
 </html>
